982bea2d9f7dc663ed62ff15b8e224d2e52c2a30
Aman Rathod <141942395+AmanRathod233@users.noreply.github.com>
2025-09-25 11:56:10 +0530

    Update authRoute.js

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,5 +1,8 @@
 import express from "express";
 import { register, login } from "../controllers/Authcontrollers.js";
+import { allowCors } from "../../../cors.js";
+
+export default allowCors(authRoute);
 
 const router = express.Router();
 
